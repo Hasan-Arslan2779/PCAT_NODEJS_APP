0cab16676cc19548f8590a69a1e81de15b718384
Hasan Arslan <hasanar2779@gmail.com>
2024-12-19 21:32:51 +0300

    Statik dosyalar yüklendi

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const fn = () => {
-  console.log('Vorking');
-};
+const express = require('express');
+const path = require('path');
+const port = 3000;
+
+const app = express();
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'temp/index.html')); // dosya göndermek için resolve yada join kullanılabilir
+});
+
+app.listen(port, () => {
+  console.log(` Sunucu ${port} portunda başlatıldı`);
+});
